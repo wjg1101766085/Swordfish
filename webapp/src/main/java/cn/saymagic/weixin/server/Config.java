286a23ffb39ae2ebb3181765a7f348dfec68ae0b
wjg1101766085 <wjg1101766085@users.noreply.github.com>
2016-08-12 15:18:53 +0800

    Update Config.java

--- webapp/src/main/java/cn/saymagic/weixin/server/Config.java
+++ webapp/src/main/java/cn/saymagic/weixin/server/Config.java
@@ -3,9 +3,9 @@ package cn.saymagic.weixin.server;
 public class Config {
 
 	/** 微信公众号的token，用于验证之用*/
-	public static final String TOKEN = "";
+	public static final String TOKEN = "wang1101766085";
 	
 	/** 图灵机器人的APPKEY，可在图灵官方申请*/
-	public static final String APIKEY = ""; 
+	public static final String APIKEY = "8e8f31db1ef449a795f38c0a011045d8"; 
 	
 }
